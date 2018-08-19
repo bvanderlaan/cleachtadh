10ac287d46704a7bdeda475b206647c0d26ea5e3
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-08-19 00:40:03 -0400

    Add custom header and footer
    This change set removes the App component and replaces it with the Root
    component as it is the _root_ of the application, then adds two new components:
      * Header: A custom Header
      * Footer: A custom Footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
