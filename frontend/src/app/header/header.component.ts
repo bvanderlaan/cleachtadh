4bfee51b75c93e9530c6b1aba560927ab76f417e
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-08-19 16:43:00 -0400

    Update expandable menu to slide out from the left
    This change set updates the expandable menu, the hamberger menu when the
    display is small, so that instead of sliding down from the top it pops out
    from the left. It also covers the main part of the page with an overlay while
    the menu is open so that it stands out more.

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+const toggleOverlay = (mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.target.classList.contains('collapsing')) {
+      return $('.overlay').addClass('show');
+    }
+
+    if (!mutation.target.classList.contains('show')) {
+      return $('.overlay').removeClass('show');
+    }
+  })
+};
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit {
+  private menuObserver;
+
+  ngOnInit() {
+    $(document).ready(() => {
+      $('.navbar-nav>li>a').on('click', () => $('.navbar-toggler').click());
+      $('.overlay').on('click', () => $('.navbar-toggler').click());
+      $('#navigation-menu-close').on('click', () => $('.navbar-toggler').click());
+
+      this.menuObserver = new MutationObserver(toggleOverlay);
+      const target = document.getElementById('navigation-menu');
+      this.menuObserver.observe(target, {
+        attributes: true,
+        attributeFilter: ['class'],
+      });
+    });
+  }
+}
