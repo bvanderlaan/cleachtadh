fb6f3b8719896241c045ba9af008cb8703fb5f78
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-08-21 00:26:52 -0400

    Add markdown support to add kata form
    This chnage set adds ngx-markdown so that when entering the kata description
    users can format the text via markdown.
    
    This includes a _preview_ tab which shows you the mark down you just typed,
    this is similar to GitHub where you can type in markdown then click the
    preview tab

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+
 import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './root/root.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { AddKataComponent } from './add-kata/add-kata.component';
+
+const markedOptions = {
+  provide: MarkedOptions,
+  useFactory() : MarkedOptions {
+    const renderer = new MarkedRenderer();
+    renderer.blockquote = (text: string) => (
+      `<blockquote class="blockquote"><p>${text}</p></blockquote>`
+    );
+
+    return {
+      renderer,
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+    };
+  },
+};
 
 @NgModule({
   declarations: [
@@ -21,6 +44,7 @@ import { AddKataComponent } from './add-kata/add-kata.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MarkdownModule.forRoot({ markedOptions }),
   ],
   providers: [],
   bootstrap: [RootComponent]
