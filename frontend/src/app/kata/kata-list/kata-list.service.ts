eebec8a2463743f5fd0c814ef45b38838c29e5f0
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-08-29 23:00:10 -0400

    :bug:Fix kata list view
    The kata list view was not showing the katas because the service was expecting
    the body of the GET to be an array but it is actually an object where the
    array is under a property of said object.
    
    This change set updates the service to map the array to the root level
    so that the component just gets the collection vs. the response body

--- frontend/src/app/kata/kata-list/kata-list.service.ts
+++ frontend/src/app/kata/kata-list/kata-list.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Response } from '@angular/http';
 
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Kata } from '../kata.model';
 import { AppSettings } from '../../app.settings';
@@ -16,10 +16,15 @@ export class KataListService {
   constructor(private http: HttpClient) { }
 
   getKatas() : Observable<Kata[]> {
-    return this.http.get<Kata[]>(`${AppSettings.API_ENDPOINT}/v1/katas`)
-      .pipe(catchError((error: Response) => {
-        console.error(`GET Katas Failed: ${error.statusText}`);
-        return of([]);
-      }));
+    return this.http.get<Katas>(`${AppSettings.API_ENDPOINT}/v1/katas`)
+      .pipe(map(data => data.katas),
+        catchError((error: Response) => {
+          console.error(`GET Katas Failed: ${error.statusText}`);
+          return of([]);
+        }));
   }
+}
+
+interface Katas {
+  katas: Kata[],
 }
