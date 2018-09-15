fd7305457f27e80c05e9d98fd92b452f5d6aea0d
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-09-14 23:00:37 -0400

    Add empty list coach text
    This change set updates the home screen so that when there are no Katas
    instead of showing a blank screen it shows text stating that there are no
    Katas and that the user should login/signup/add a kata

--- frontend/src/app/kata/kata-list/kata-list.component.ts
+++ frontend/src/app/kata/kata-list/kata-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Kata } from '../kata.model';
 import { KataListService } from './kata-list.service';
+import { AuthenticationService } from '../../authentication';
 
 @Component({
   selector: 'kata-list',
@@ -10,13 +11,19 @@ import { KataListService } from './kata-list.service';
 })
 export class KataListComponent implements OnInit {
   private katas: Kata[];
+  public loggedIn: boolean;
 
-  constructor(private service: KataListService) {
+  constructor(private service: KataListService, private authService: AuthenticationService) {
     this.katas = [];
+    this.loggedIn = false;
   }
 
   ngOnInit() {
     this.service.getKatas().subscribe(katas => (this.katas = katas));
+
+    this.authService.userLogInStateSignal.subscribe((currentUserDisplayName) => {
+      this.loggedIn = !!currentUserDisplayName;
+    });
   }
 
 }
