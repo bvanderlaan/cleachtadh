be3a11b60aa2e9478f1af2fd2625815237cead47
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-09-18 22:13:29 -0400

    :green_heart:Fix failing benchmark tests
    When I added authorization guards to the mutative routes (POST, PUT, PATCH,
    DESTROY) I forgot to update the benchmark tests to include an auth token
    so they were failing with 401 errors.
    
    This change set updates the benchmark tests to create a user then include
    that users auth token when testing the mutative routes

--- backend/test/katas/katas.bench.js
+++ backend/test/katas/katas.bench.js
@@ -1,16 +1,34 @@
 'use strict';
 
 const request = require('superagent');
+const uuid = require('uuid/v4');
 
 const server = require('../index.bench');
 
+const createUser = (...{ displayName = 'Rocket Racoon', email = `rocket+${uuid()}@gmail.com` }) => (
+  request.post(`${server.url}/v1/signup`)
+    .send({
+      displayName,
+      email,
+      password: 'password',
+    })
+    .then(response => response.body.token)
+);
+
 suite('Katas Route', () => {
   let kataIds = [];
+  let token;
+
+  before(() => (
+    createUser()
+      .then(userToken => (token = userToken))
+  ));
 
   before(() => {
     const katas = Array.from(new Array(100), (_, i) => i)
       .map((_, i) => (
         request.post(`${server.url}/v1/katas`)
+          .set('Authorization', `Bearer ${token}`)
           .send({
             name: `my first system kata (${i})`,
             description: 'this is how we do it',
@@ -26,6 +44,7 @@ suite('Katas Route', () => {
   after(() => {
     const requests = kataIds.map(kataId => (
       request.delete(`${server.url}/v1/katas/${kataId}`)
+        .set('Authorization', `Bearer ${token}`)
     ));
     return Promise.all(requests);
   });
